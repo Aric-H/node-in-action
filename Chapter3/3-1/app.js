39c78b8d7fd1940e2e6197fb3cc88edd1e222f3a
Aric-H <heji@szboanda.net>
2015-11-18 09:14:40 +0800

    add chapter3-1 example

---
+++ Chapter3/3-1/app.js
@@ -0,0 +1,33 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req,res){
+	getTitle(res);
+}).listen(8000,'127.0.0.1');
+
+function getTitle(res){
+	fs.readFile('./title.json',function(err,data){
+		if(err)
+			return hadError(err,res);
+		getTemplate(JSON.parse(data.toString()),res);
+	})
+}
+
+function hadError(err,res){
+	console.error(err);
+	res.end('Server Error');
+}
+
+function getTemplate(titles,res){
+	fs.readFile('./template.html',function(err,data){
+		if(err)
+			return hadError(err,res);
+		formatHtml(titles,data.toString(),res);
+	})
+}
+
+function formatHtml(titles,tmpl,res){
+	var html = tmpl.replace('%',titles.join('</li><li>'));
+	res.writeHead(200,{'Content-Type':'text/html'});
+	res.end(html);
+}
